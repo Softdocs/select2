fcd5146d01db98ef2700a7f20d42de2dab15aac0
Taylor Miller <millertn@etsu.edu>
2021-08-03 11:51:31 -0400

    throw in some console.logs and switch prop with attr

--- select2.js
+++ select2.js
@@ -2606,10 +2606,11 @@ the specific language governing permissions and limitations under the Apache Lic
             if (this.showSearchInput === showSearchInput) return;
 
             this.showSearchInput = showSearchInput;
-            var tabindex = !showSearchInput ? -1 : 0 
-
+            var tabindex = !showSearchInput ? '0' : '-1';
+            console.log(tabindex);
+            console.log(this.dropdown.find(".select2-search"));
             this.dropdown.find(".select2-search").toggleClass("select2-search-hidden", !showSearchInput);
-            this.dropdown.find(".select2-offscreen").attr("tabindex", tabindex);
+            this.dropdown.find(".select2-search").prop("tabindex", tabindex);
             this.dropdown.find(".select2-search").toggleClass("select2-offscreen", !showSearchInput);
             //add "select2-with-searchbox" to the container if search box is shown
             $(this.dropdown, this.container).toggleClass("select2-with-searchbox", showSearchInput);
